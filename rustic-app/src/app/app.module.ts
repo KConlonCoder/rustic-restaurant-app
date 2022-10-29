ac414ed278e5408bd23af38c7cafa30c6fb6e996
KConlonCoder <katie.e.conlon@gmail.com>
2022-10-29 16:59:38 -0400

    Removed angular router import related to an error

--- rustic-app/src/app/app.module.ts
+++ rustic-app/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 // import { NavComponent } from './components/nav/nav.component';
